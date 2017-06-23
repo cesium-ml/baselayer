895dbda81659e90b34ee61cf0d0eaf7df7b05006
Stefan van der Walt <stefanv@berkeley.edu>
2017-06-22 20:58:06 -0700

    Print database and role creation output

--- tools/db_init.py
+++ tools/db_init.py
@@ -6,20 +6,17 @@ import glob
 import os
 from baselayer.app.config import load_config
 
-
-parent_yaml_files = glob.glob('../../*.yaml*') + glob.glob('*.yaml*')
-
-parent_yaml_files = ([os.path.abspath(path) for path in parent_yaml_files] if
-                     parent_yaml_files else None)
+cfg = load_config()
+db = cfg['database:database']
+user = cfg['database:user']
 
 
-cfg = load_config(parent_yaml_files)
-
-p = subprocess.run(['./baselayer/tools/db_init.sh', cfg['database:database'],
-                    cfg['database:user']],
+print(f'$ ./baselayer/tools/db_init.sh {db} {user}')
+p = subprocess.run(['./baselayer/tools/db_init.sh', db, user],
                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-if p.returncode != 0:
-    print('stdout:', p.stdout)
-    print('stderr:', p.stderr)
-    sys.exit(p.returncode)
+print('--- stdout ---')
+print(p.stdout.decode('utf-8').strip())
+print('--- stderr ---')
+print(p.stderr.decode('utf-8').strip())
+sys.exit(p.returncode)
