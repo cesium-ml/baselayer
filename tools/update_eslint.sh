017464c7a7c00e74922d76306c04beda384a6918
Stefan van der Walt <stefanv@berkeley.edu>
2017-08-16 16:37:09 -0700

    Add eslint script and updater

---
+++ tools/update_eslint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export PKG=eslint-config-airbnb
+npm info "${PKG}@latest" peerDependencies --json | \
+    command sed 's/[\{\},]//g ; s/: /@/g' | \
+    xargs npm install --save-dev "${PKG}@latest"
