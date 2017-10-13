3ebd17d39723c9fe783ed8d225354a3f907576fb
Ari Crellin-Quick <a.crellinquick@gmail.com>
2017-10-12 18:47:48 -0700

    Add auth_or_token decorator

--- app/access.py
+++ app/access.py
@@ -1,5 +1,7 @@
 import functools
 import tornado.web
+import jwt
+from baselayer.app.custom_exceptions import AccessError
 from baselayer.app.models import Role, User
 
 
@@ -16,3 +18,24 @@ def permissions(acl_list):
             return method(self, *args, **kwargs)
         return wrapper
     return check_acls
+
+
+def auth_or_token(method):
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        cookies_dict = {k: morsel.value for k, morsel in self.request.cookies.items()}
+        if 'token' in cookies_dict:
+            try:
+                raw_token = jwt.decode(cookies_dict['token'],
+                                       self.cfg['app:secret-key'])['token']
+            except (jwt.exceptions.DecodeError, KeyError):
+                raise AccessError('Invalid auth token in request cookies')
+            matching_users = list(User.query.filter(User.username == raw_token))
+            if len(matching_users) == 1:
+                self.current_user = matching_users[0]
+            else:
+                raise tornado.web.HTTPError(403)
+            return method(self, *args, **kwargs)
+        else:
+            return tornado.web.authenticated(method)(self, *args, **kwargs)
+    return wrapper
