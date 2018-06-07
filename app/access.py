6d412d2b2bdb83694de049d052ba5c4e2418cba2
Ari Crellin-Quick <a.crellinquick@gmail.com>
2018-06-07 14:27:45 -0700

    Use auth_or_token decorator in permissions decorator instead of tornado.web.authenticated

--- app/access.py
+++ app/access.py
@@ -4,21 +4,6 @@ from baselayer.app.custom_exceptions import AccessError
 from baselayer.app.models import Role, User, Token
 
 
-def permissions(acl_list):
-    """Decorate methods with this to require that the current user have all the
-    given ACLs.
-    """
-    def check_acls(method):
-        @tornado.web.authenticated
-        @functools.wraps(method)
-        def wrapper(self, *args, **kwargs):
-            if not set(acl_list).issubset(self.current_user.permissions):
-                raise tornado.web.HTTPError(403)
-            return method(self, *args, **kwargs)
-        return wrapper
-    return check_acls
-
-
 def auth_or_token(method):
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
@@ -33,3 +18,18 @@ def auth_or_token(method):
         else:
             return tornado.web.authenticated(method)(self, *args, **kwargs)
     return wrapper
+
+
+def permissions(acl_list):
+    """Decorate methods with this to require that the current user have all the
+    specified ACLs.
+    """
+    def check_acls(method):
+        @auth_or_token
+        @functools.wraps(method)
+        def wrapper(self, *args, **kwargs):
+            if not set(acl_list).issubset(self.current_user.permissions):
+                raise tornado.web.HTTPError(403)
+            return method(self, *args, **kwargs)
+        return wrapper
+    return check_acls
