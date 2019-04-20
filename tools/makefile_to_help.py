3805649c74861bcad414f6ee4fa56a3493966b0c
Stefan van der Walt <stefanv@berkeley.edu>
2019-04-19 17:12:43 -0700

    makefile-to-help: add headers to baselayer/skyportal target categories

--- tools/makefile_to_help.py
+++ tools/makefile_to_help.py
@@ -1,5 +1,14 @@
 """
 Convert any ## style comments after a Makefile target into help text.
+
+Usage: makefile_to_help.py <MAKEFILE0> <MAKEFILE1> ...
+
+The Makefile can also be preceded by a category, e.g.
+
+  makefile_to_help.py Main:Makefile External:submodule/Makefile
+
+in which case the category names are printed as a heading before the targets.
+
 """
 
 import sys
@@ -9,16 +18,23 @@ if not sys.argv:
     print("Usage: makefile_to_help.py <MAKEFILE0> <MAKEFILE1> ...")
     sys.exit(0)
 
-lines = []
-for fname in sys.argv[1:]:
-    with open(fname) as f:
-        lines.extend(f.readlines())
 
-matches = [re.match('^([\w-]+): +##(.*)', line) for line in lines]
-groups = [m.groups(0) for m in matches if m]
-targets = {target: desc for (target, desc) in groups}
+def describe_targets(lines):
+    matches = [re.match('^([\w-]+): +##(.*)', line) for line in lines]
+    groups = [m.groups(0) for m in matches if m]
+    targets = {target: desc for (target, desc) in groups}
 
-N = max(len(target) for (target, desc) in targets.items())
+    N = max(len(target) for (target, desc) in targets.items())
 
-for (target, desc) in targets.items():
-    print(f'{target:{N}} {desc}')
+    for (target, desc) in targets.items():
+        print(f'{target:{N}} {desc}')
+
+
+for source in sys.argv[1:]:
+    if ':' in source:
+        category, fname = source.split(':')
+        print(f'\n{category}\n{"-" * len(category)}')
+    else:
+        fname = source
+
+    describe_targets(open(fname).readlines())
