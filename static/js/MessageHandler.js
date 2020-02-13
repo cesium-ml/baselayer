405e67e43d44a5b8ad83179d67b7f24f4dd41ee2
Stefan van der Walt <stefanv@berkeley.edu>
2020-02-12 16:37:52 -0800

    Refactor MessageHandler
    There is now a singleton `messageHandler` object.  To register a
    handler, use `messageHandler.add(handler)`.  The handler should be a
    function of the form:
    
    ```
    (action, payload, dispatch, getState) => { ... }
    ```

--- static/js/MessageHandler.js
+++ static/js/MessageHandler.js
@@ -1,19 +1,42 @@
+/* Handle incoming websocket messages */
+
 import { SHOW_NOTIFICATION, showNotification } from './components/Notifications';
 
 class MessageHandler {
-  constructor(dispatch, handler) {
-    this.dispatch = dispatch;
-    this.handle = message => {
-      switch (message.action) {
-        case SHOW_NOTIFICATION:
-          this.dispatch(showNotification(message.payload.note,
-                                         message.payload.type));
-          break;
-        default:
-          handler(message);
-      }
-    };
+  /* You have to run `init` before the messageHandler can be used */
+
+  constructor() {
+    this._handlers = [];
+    this._dispatch = null;
+    this._getState = null;
+  }
+
+  init(dispatch, getState) {
+    this._dispatch = dispatch;
+    this._getState = getState;
+  }
+
+  add(handler) {
+    this._handlers.push(handler);
+  }
+
+  handle(action, payload) {
+    // Execute all registered handlers on the incoming message
+    this._handlers.forEach((handler) => {
+      handler(action, payload, this._dispatch, this._getState);
+    });
   }
 }
 
-export default MessageHandler;
+const notificationHandler = (action, payload, dispatch) => {
+  if (action === SHOW_NOTIFICATION) {
+    const { note, type } = payload;
+    dispatch(showNotification(note, type));
+  }
+};
+
+
+const messageHandler = new MessageHandler();
+messageHandler.add(notificationHandler);
+
+export default messageHandler;
