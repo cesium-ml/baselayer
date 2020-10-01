c26241aa02d2201660c3a39bfe3096347053634d
Stefan van der Walt <stefanv@berkeley.edu>
2020-10-01 13:54:52 -0700

    Watch app_00.log by default
    This is a stop-gap measure: we should build in detection for files as
    they appear, but for now this will at least ensure that one
    application log is captured.

--- tools/watch_logs.py
+++ tools/watch_logs.py
@@ -45,7 +45,7 @@ watched.append('log/error.log')
 watched.append('log/nginx-bad-access.log')
 watched.append('log/nginx-error.log')
 watched.append('log/fake_oauth2.log')
-
+watched.append('log/app_00.log')
 
 def tail_f(filename, interval=1.0):
     f = None
