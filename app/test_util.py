3b93dcc7137b0e96da11eb9e5aa505e599f3c1bb
Danny Goldstein <dgold@berkeley.edu>
2020-10-30 16:15:12 -0700

    Redirect webdriven firefox console logs to geckodriver logs (#173)

--- app/test_util.py
+++ app/test_util.py
@@ -150,6 +150,7 @@ def driver(request):
     options = webdriver.FirefoxOptions()
     if "BASELAYER_TEST_HEADLESS" in os.environ:
         options.headless = True
+    options.set_preference('devtools.console.stdout.content', True)
 
     profile = webdriver.FirefoxProfile()
     profile.set_preference("browser.download.manager.showWhenStarting", False)
@@ -165,7 +166,10 @@ def driver(request):
         ),
     )
 
-    driver = MyCustomWebDriver(firefox_profile=profile, options=options)
+    driver = MyCustomWebDriver(
+        firefox_profile=profile,
+        options=options,
+    )
     driver.set_window_size(1920, 1200)
     login(driver)
 
