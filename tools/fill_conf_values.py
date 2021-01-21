9c95c78576be748c612ee6574e8aac01b3f1041b
Stefan van der Walt <stefanv@berkeley.edu>
2021-01-21 15:14:35 -0800

    Add md5sum filter to jinja templating (#183)

--- tools/fill_conf_values.py
+++ tools/fill_conf_values.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import re
 import os
 from status import status
 
@@ -12,19 +11,26 @@ import jinja2
 log = make_log('baselayer')
 
 
+def md5sum(fn):
+    import hashlib
+    with open(fn, "rb") as f:
+        file_hash = hashlib.md5()
+        while chunk := f.read(8192):
+            file_hash.update(chunk)
+    return file_hash.hexdigest()
+
+
 def fill_config_file_values(template_paths):
     log('Compiling configuration templates')
     env, cfg = load_env()
 
     for template_path in template_paths:
         with status(template_path):
-            with open(template_path) as f:
-                data = f.read()
-
             tpath, tfile = os.path.split(template_path)
             env = jinja2.Environment(
                 loader=jinja2.FileSystemLoader(tpath),
             )
+            env.filters['md5sum'] = md5sum
             template = env.get_template(tfile)
             rendered = template.render(cfg)
 
