28f153ff3261d5b8fdfd6b616f550f6b409c2e55
Stefan van der Walt <stefanv@berkeley.edu>
2021-04-14 12:49:17 -0700

    Make database engine creation defaults adjustable.
    - `pool_size`:
       The number of connections maintained to the DB.
    
    - `max_overflow`:
       The number of additional connections that will be made as needed.
       Once these extra connections have been used, they are discarded.
    
    - `pool_recycle`:
       Prevent the pool from using any connection that is older than this
       (specified in seconds).
    
    See also https://docs.sqlalchemy.org/en/14/core/pooling.html#sqlalchemy.pool.QueuePool.__init__

--- app/models.py
+++ app/models.py
@@ -68,16 +68,39 @@ utcnow = func.timezone("UTC", func.current_timestamp())
 # The db has to be initialized later; this is done by the app itself
 # See `app_server.py`
 def init_db(
-    user, database, password=None, host=None, port=None, autoflush=True
+        user, database, password=None, host=None, port=None, autoflush=True,
+        engine_args={}
 ):
+    """
+    Parameters
+    ----------
+    engine_args : dict
+        - `pool_size`:
+          The number of connections maintained to the DB. Default 5.
+
+        - `max_overflow`:
+          The number of additional connections that will be made as needed.
+           Once these extra connections have been used, they are discarded.
+          Default 10.
+
+        - `pool_recycle`:
+           Prevent the pool from using any connection that is older than this
+           (specified in seconds).
+           Default 3600.
+
+    """
     url = "postgresql://{}:{}@{}:{}/{}"
     url = url.format(user, password or "", host or "", port or "", database)
 
+    default_engine_args = {
+            'pool_size': 5, 'max_overflow': 10, 'pool_recycle': 3600
+        }
     conn = sa.create_engine(
         url,
         client_encoding="utf8",
         executemany_mode="values",
         executemany_values_page_size=EXECUTEMANY_PAGESIZE,
+        **{**default_engine_args, **engine_args}
     )
 
     DBSession.configure(bind=conn, autoflush=autoflush)
