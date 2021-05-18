73775710579940ae777d2488429c1f90f6510ea3
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-05-17 17:42:11 -0700

    Revert "Simplify access controls for system admins (#225)"
    This reverts commit b1b9e1ccaa88edf3ea820a0f0d8467cc169348a3.

--- app/models.py
+++ app/models.py
@@ -68,13 +68,8 @@ utcnow = func.timezone("UTC", func.current_timestamp())
 # The db has to be initialized later; this is done by the app itself
 # See `app_server.py`
 def init_db(
-    user,
-    database,
-    password=None,
-    host=None,
-    port=None,
-    autoflush=True,
-    engine_args={},
+        user, database, password=None, host=None, port=None, autoflush=True,
+        engine_args={}
 ):
     """
     Parameters
@@ -98,16 +93,14 @@ def init_db(
     url = url.format(user, password or "", host or "", port or "", database)
 
     default_engine_args = {
-        "pool_size": 5,
-        "max_overflow": 10,
-        "pool_recycle": 3600,
-    }
+            'pool_size': 5, 'max_overflow': 10, 'pool_recycle': 3600
+        }
     conn = sa.create_engine(
         url,
         client_encoding="utf8",
         executemany_mode="values",
         executemany_values_page_size=EXECUTEMANY_PAGESIZE,
-        **{**default_engine_args, **engine_args},
+        **{**default_engine_args, **engine_args}
     )
 
     DBSession.configure(bind=conn, autoflush=autoflush)
@@ -481,12 +474,6 @@ class AccessibleIfRelatedRowsAreAccessible(UserAccessControl):
             Query for the accessible rows.
         """
 
-        # system admins automatically get full access
-        if user_or_token.is_admin:
-            return public.query_accessible_rows(
-                cls, user_or_token, columns=columns
-            )
-
         # only return specified columns if requested
         if columns is None:
             base = DBSession().query(cls)
@@ -608,11 +595,6 @@ class ComposedAccessControl(UserAccessControl):
         query: sqlalchemy.Query
             Query for the accessible rows.
         """
-        # system admins automatically get full access
-        if user_or_token.is_admin:
-            return public.query_accessible_rows(
-                cls, user_or_token, columns=columns
-            )
 
         # retrieve specified columns if requested
         if columns is not None:
