db40a0688351653c5c12a7adaee5eb79856ae806
kshin <kshin@caltech.edu>
2021-06-25 15:55:43 -0400

    Use existing is_active function for user expiration

--- app/access.py
+++ app/access.py
@@ -19,16 +19,20 @@ def auth_or_token(method):
 
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
-        token_header = self.request.headers.get('Authorization', None)
-        if token_header and token_header.startswith('token '):
-            token_id = token_header.replace('token', '').strip()
+        token_header = self.request.headers.get("Authorization", None)
+        if token_header and token_header.startswith("token "):
+            token_id = token_header.replace("token", "").strip()
             token = Token.query.get(token_id)
             if token is not None:
                 self.current_user = token
+                if not token.created_by.is_active():
+                    raise tornado.web.HTTPError(403, "User account expired")
             else:
                 raise tornado.web.HTTPError(403)
             return method(self, *args, **kwargs)
         else:
+            if not self.current_user.is_active():
+                raise tornado.web.HTTPError(403, "User account expired")
             return tornado.web.authenticated(method)(self, *args, **kwargs)
 
     return wrapper
