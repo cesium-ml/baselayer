ee504eed8f03770ca448c73a318f2fc2ff44b65e
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-10-19 15:35:23 -0700

    Run webpack in --watch mode in production

--- services/webpack/webpack.py
+++ services/webpack/webpack.py
@@ -26,5 +26,5 @@ if env.debug:
     sys.exit(p.returncode)
 else:
     log("Rebuilding main JavaScript bundle")
-    p = run(['npx', 'webpack', '--mode=production', '--devtool=none'])
+    p = run(['npx', 'webpack', '--watch', '--mode=production', '--devtool=none'])
     sys.exit(p.returncode)
