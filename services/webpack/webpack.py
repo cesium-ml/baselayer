350e9ec8248af9c642bd79bb9ae7052f0bbe9596
Ari Crellin-Quick <a.crellinquick@gmail.com>
2021-12-09 10:38:08 -0800

    Upgrade webpack command flags for v5-compatibility

--- services/webpack/webpack.py
+++ services/webpack/webpack.py
@@ -6,25 +6,25 @@ import sys
 from baselayer.app.env import load_env, parser
 from baselayer.log import make_log
 
-parser.description = 'Launch webpack microservice'
+parser.description = "Launch webpack microservice"
 
 env, cfg = load_env()
 
-log = make_log('service/webpack')
+log = make_log("service/webpack")
 
 
 def run(cmd):
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     for line in proc.stdout:
-        log(f'{line.decode().strip()}')
+        log(f"{line.decode().strip()}")
     return proc
 
 
 if env.debug:
     log("debug mode detected, launching webpack monitor")
-    p = run(['npx', 'webpack', '--watch'])
+    p = run(["npx", "webpack", "--watch"])
     sys.exit(p.returncode)
 else:
     log("Rebuilding main JavaScript bundle")
-    p = run(['npx', 'webpack', '--watch', '--mode=production', '--devtool=none'])
+    p = run(["npx", "webpack", "--watch", "--mode=production"])
     sys.exit(p.returncode)
