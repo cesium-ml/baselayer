5d79589c3e814becd3064b2d3aeb742250b790fb
Stefan van der Walt <stefanv@berkeley.edu>
2022-03-02 00:17:14 -0800

    Pull back node requirement to 16.14

--- tools/check_app_environment.py
+++ tools/check_app_environment.py
@@ -26,7 +26,7 @@ deps = {
     ),
     'psql': (['psql', '--version'], lambda v: v.split('\n')[-1].split()[2], '12.0',),
     'npm': (['npm', '-v'], lambda v: v, '8.3.2'),
-    'node': (['node', '-v'], lambda v: v[1:], '17.3.0'),
+    'node': (['node', '-v'], lambda v: v[1:], '16.14.0'),
     'python': (['python', '--version'], lambda v: v.split()[1], '3.8'),
 }
 
