3e294d5b7164fea5a78b1c846c75833a57013126
jbloom <profjsb@gmail.com>
2022-09-05 18:43:59 -0700

    dont require nodejs 18

--- tools/check_app_environment.py
+++ tools/check_app_environment.py
@@ -30,7 +30,7 @@ deps = {
         "12.0",
     ),
     "npm": (["npm", "-v"], lambda v: v, "8.3.2"),
-    "node": (["node", "-v"], lambda v: v[1:], "18.2.0"),
+    "node": (["node", "-v"], lambda v: v[1:], "16.14.0"),
     "python": (["python", "--version"], lambda v: v.split()[1], "3.8"),
 }
 
