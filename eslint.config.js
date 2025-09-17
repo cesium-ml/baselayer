270d0345677540bce695eb1c8a64251f570765ab
Stefan van der Walt <stefanv@berkeley.edu>
2025-09-17 02:50:07 -0700

    Update eslint config

---
+++ eslint.config.js
@@ -0,0 +1,79 @@
+// eslint.config.js
+const global = require("globals");
+const eslint = require("@eslint/js");
+
+const reactPlugin = require("eslint-plugin-react");
+const prettierPlugin = require("eslint-config-prettier");
+const importPlugin = require("eslint-plugin-import");
+const reactHookPlugin = require("eslint-plugin-react-hooks");
+const airbnbPlugin = require("eslint-config-airbnb");
+
+const { fixupPluginRules } = require("@eslint/compat");
+
+module.exports = [
+  eslint.configs.recommended,
+  // run on all js and jsx files in the static directory and subdirectories
+  { files: ["**/*.js", "**/*.jsx"] },
+  {
+    languageOptions: {
+      parser: require("@babel/eslint-parser"),
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...global.browser,
+      },
+    },
+  },
+  {
+    plugins: {
+      import: importPlugin,
+      react: reactPlugin,
+      "react-hooks": fixupPluginRules(reactHookPlugin),
+      airbnb: airbnbPlugin,
+      prettier: prettierPlugin,
+    },
+  },
+  {
+    rules: {
+      ...reactHookPlugin.configs.recommended.rules,
+      ...prettierPlugin.rules,
+      camelcase: "off",
+      "no-unused-vars": "off",
+      "no-unsafe-optional-chaining": "off",
+      "no-useless-escape": "off",
+      "no-constant-binary-expression": "warn",
+      "jsx-a11y/click-events-have-key-events": 0,
+      "jsx-a11y/label-has-associated-control": 0,
+      "jsx-a11y/control-has-associated-label": 0,
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/jsx-wrap-multilines": 0,
+      "react/jsx-one-expression-per-line": 0,
+      "react/jsx-props-no-spreading": 0,
+      "react/jsx-curly-newline": 0,
+      "no-param-reassign": 0,
+    },
+  },
+  {
+    settings: {
+      import: {
+        resolver: {
+          node: {},
+          rspack: {
+            config: "rspack.config.js",
+          },
+        },
+      },
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
