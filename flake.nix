f9a65f84b5ab584029df587ecfd6e5c512b5891c
Leo Conforti <leo@leoconforti.us>
2026-07-13 17:06:34 -0500

    cleanup flake

--- flake.nix
+++ flake.nix
@@ -13,20 +13,29 @@
     in
     {
       formatter = forAllSystems (pkgs: pkgs.alejandra);
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            nixd
-            nixfmt
-            python3
-            uv
-          ];
-        };
+      devShells = forAllSystems (
+        pkgs:
+        let
+          nodejs = pkgs.nodejs-slim_latest;
+          corepack = pkgs.corepack.override { nodejs-slim = nodejs; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nixd
+              nixfmt
+              nodejs
+              corepack
+              python3
+              uv
+            ];
 
-        shellHook = ''
-          unset PYTHONPATH
-          uv sync
-        '';
-      });
+            shellHook = ''
+              unset PYTHONPATH
+              uv sync
+            '';
+          };
+        }
+      );
     };
 }
