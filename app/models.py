9f9b170c8163bd1d94a7a6258e17f6c5005784b9
antoine-le-calloch <antoine.lecalloch@gmail.com>
2026-09-11 17:28:31 -0500

    Return a plain session from VerifiedSession, the scoped wrapper did nothing

--- app/models.py
+++ app/models.py
@@ -93,14 +93,7 @@ class _VerifiedSession(sa.orm.session.Session):
 
 
 def VerifiedSession(user_or_token):
-    return scoped_session(
-        sessionmaker(
-            class_=_VerifiedSession,
-            user_or_token=user_or_token,
-            bind=db_engine(),
-        ),
-        scopefunc=session_context_id.get,
-    )()
+    return _VerifiedSession(user_or_token, bind=db_engine())
 
 
 def db_engine():
